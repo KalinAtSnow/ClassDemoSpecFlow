477db373a846f24a2b9a86dd92e1106487b4b3ff
kalin.nebeker <kalin.nebeker@yahoo.com>
2022-10-12 11:51:28 -0600

    fixed tests, no longer has a main

--- Calculator.Main/Program.cs
+++ Calculator.Main/Program.cs
@@ -1,8 +1,1 @@
 ﻿
-Console.WriteLine("hello there, welcome to the calculator program where you can add or multiply");
-Console.WriteLine("type 1 if you want to add or 2 if you want to multiply");
-string input = Console.ReadLine();
-if (input == "1")
-{
-    new calculator
-}
